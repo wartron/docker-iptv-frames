0691103d80bc23c11b33caaeb85864e5b12f8e2b
Will Wharton <willmwharton@gmail.com>
2016-01-03 18:31:24 -0800

    python telnet interface with snapshot

--- capture.py
+++ capture.py
@@ -1,1 +1,10 @@
-capture.py
+from vlcclient import VLCClient
+vlc = VLCClient("::1")
+vlc.connect()
+
+
+
+stream = "http://nhkwglobal-i.akamaihd.net/hls/live/222714/nhkwglobal/index_1180.m3u8"
+
+
+vlc.add(stream)
---
+++ launchvlc.sh
@@ -0,0 +1,9 @@
+vlc --intf telnet --telnet-password admin --noaudio --video-filter=scene --vout=dummy > vlc.log &
+
+
+
+
+
+
+
+vlc --noaudio --vout=dummy
