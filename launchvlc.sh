f4f7f25eca8d2f25373befbc7f78efc1fa5b0130
Will Wharton <willmwharton@gmail.com>
2016-01-03 19:41:07 -0800

    set vlc snapshot output dir

--- launchvlc.sh
+++ launchvlc.sh
@@ -2,7 +2,7 @@
 
 
 
-vlc --intf telnet --telnet-password admin --noaudio --vout=dummy > vlc.log &
+nohup vlc --intf telnet --telnet-password admin --noaudio --vout=dummy --snapshot-path="out" &
 
 
 
